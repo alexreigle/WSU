1e6d3788da0ab0b8d65202f971dca1fe186864b5
Alex Reigle <alex.reigle@brilligent.com>
2021-11-09 22:34:49 -0500

    Started Project 7

---
+++ CEG7580/Project7/main.m
@@ -0,0 +1,27 @@
+% Project 6
+% CEG 7850 - Digital Image Processing
+% Alex Reigle
+
+function [] = main()
+path = pwd;
+
+% Problem 1: Salt & Pepper Noise Generation/Median Filter based Noise Reduction
+% (a) Add salt-pepper noise to Fig. 5.7(a) from text with Pa=Pb= 0.2
+%     eqn. (5-16) p(z) = { Pa       ;  for z = 2^(k)-1
+%                        { Pb       ;  for z = 0
+%                        { 1-(Pa+Pb);  for z = V
+
+
+% (b) Apply median filtering block (3x3) to new image.
+
+% (c) Apply two more passes of median filtering and display results
+
+% (d) Explain any major differences
+
+% Problem 2: Periodic Noise Reduction Using a Notch Filter
+% (a) Remove sinusoidal interference in frequency domain from astronaut.tif
+
+% (b) Display and compare to original image
+
+
+end
